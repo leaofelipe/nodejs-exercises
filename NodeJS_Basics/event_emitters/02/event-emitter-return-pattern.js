38f1b3282d300ec9bd29cc091448ee51af7506b0
Felipe Leão <felipe@felipeleao.com>
2017-02-23 13:41:19 -0300

    chore: event emitter return pattern

---
+++ NodeJS_Basics/event_emitters/02/event-emitter-return-pattern.js
@@ -0,0 +1,40 @@
+/**
+* Event Emitter Return Pattern
+*/
+
+const EventEmitter = require('events').EventEmitter
+
+let randomNumber = Math.floor(Math.random() * 20)
+
+function getSource (max) {
+  let emitter = new EventEmitter()
+
+  process.nextTick(function () {
+    let count = 0
+    emitter.emit('start')
+
+    let t = setInterval(function () {
+      emitter.emit('data', ++count)
+      if (count === max) {
+        emitter.emit('end', count)
+        clearInterval(t)
+      }
+    }, 10)
+  })
+
+  return emitter
+}
+
+let res = getSource(randomNumber)
+
+res.on('start', () => {
+  console.log('Get Source Started... ')
+})
+
+res.on('data', (data) => {
+  console.log('Data -> ', data)
+})
+
+res.on('end', (amount) => {
+  console.log(`Get Source ends with ${amount}  events`)
+})
