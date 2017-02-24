76606902c070fe8536a09d64e0854014cee1675b
Felipe Leão <felipe@felipeleao.com>
2017-02-24 00:11:20 -0300

    fix: update next-tick file

---
+++ NodeJS_Basics/asynchronous/next-tick-01.js
@@ -0,0 +1,18 @@
+/**
+* Node NextTick
+*/
+
+function callme () {
+  console.log('Tick A')
+}
+
+function callme2 () {
+  console.log('Tick D')
+  process.nextTick(function () {
+    console.log('Tick B')
+  })
+}
+
+process.nextTick(callme)
+callme2()
+console.log('Tick C')
