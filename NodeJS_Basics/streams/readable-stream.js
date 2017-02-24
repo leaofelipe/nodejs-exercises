77c1cfbc25cc56b4e8a529f32c0ab158d1308f7b
Felipe Leão <felipe@felipeleao.com>
2017-02-24 00:13:12 -0300

    feat: readable stream

---
+++ NodeJS_Basics/streams/readable-stream.js
@@ -0,0 +1,38 @@
+/**
+* Readable Stream
+**/
+
+const request = require('request')
+
+let stream = request({
+  headers: {
+    'User-Agent': 'nodejs'
+  },
+  'url': 'https://api.github.com/orgs/nodejs/repos'
+})
+
+let requestLength
+let dataReceived = 0
+
+if (!stream.readable) {
+  console.log('This stream is not readable.')
+  process.exit()
+}
+
+function update (data) {
+  dataReceived += data.length
+  let percent = Math.round((dataReceived * 100) / requestLength)
+  console.log(`${data.length} (${percent}%) of ${requestLength} (100%)`)
+}
+
+stream.on('response', function (data) {
+  requestLength = data.headers['content-length']
+})
+stream.on('data', update)
+stream.on('end', function () {
+  console.log('Stream completed!')
+})
+
+stream.on('error', function (error) {
+  console.log(error)
+})
--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 ##### NodeJS basics:
 * [Asynchronous Code](NodeJS_Basics/asynchronous/)  
 * [Event Emitters](NodeJS_Basics/event_emitters/)  
+* [Streams](NodeJS_Basics/streams/)
 
 [![Standard - JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
