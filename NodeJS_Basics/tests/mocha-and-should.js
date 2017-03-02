0c33ebf3cbe6e2e26aa859b4c64f6d3ba18da656
Felipe Leão <felipe@felipeleao.com>
2017-03-01 22:55:31 -0300

    feat: include mocha as Node Enviroment

--- NodeJS_Basics/tests/mocha-and-should.js
+++ NodeJS_Basics/tests/mocha-and-should.js
@@ -4,37 +4,33 @@
 const should = require('should')
 let mf = require('./mathFunctions')
 
-describe('MathFun', function () {
-  describe('when used syncronously', function () {
-    it ('should double even numbers correctly', function () {
+describe('MathFun', () => {
+  describe('when used syncronously', () => {
+    it('should double even numbers correctly', () => {
       mf.evenDoublerSync(2).should.equal(4)
     })
 
-    it ('should throw on odd numbers', function (done) {
+    it('should throw on odd numbers', (done) => {
       (function () { mf.evenDoublerSync(3) }).should.throw(/Odd/)
       done()
     })
   })
 
-  describe('when used asyncronously', function () {
-    it.skip ('should double even numbers correctly', function (done) { //skip or ONLY
-      mf.evenDoublerAsync(2, function (err, results) {
+  describe('when used asyncronously', () => {
+    it.skip('should double even numbers correctly', (done) => { // skip or ONLY
+      mf.evenDoublerAsync(2, (err, results) => {
         should.not.exist(err) // err.should.not..... ERROR IS UNDEFINED
         results.should.equal(4)
         done()
-      }) 
+      })
     })
-  
-    it ('should return error on odd number', function (done) {
-      mf.evenDoublerAsync(3, function (err, results) {
+
+    it('should return error on odd number', (done) => {
+      mf.evenDoublerAsync(3, (err, results) => {
         should.exist(err)
         should.not.exist(results)
         done()
       })
     })
-
   })
-
-
-
 })
